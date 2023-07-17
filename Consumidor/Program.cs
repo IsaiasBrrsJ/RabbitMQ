c45bf87ea5f42ba0be08a21563a9a1501b674baf
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-07-17 07:31:17 -0400

    RabbitMQ
    Estudos sobre rabbitMQ

---
+++ Consumidor/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Publicador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Servidor RabbitMq
+            var servidor = new ConnectionFactory() 
+            {   HostName = "localhost", 
+                Port = 562, 
+                UserName = "usuario", 
+                Password = "Senha@123" 
+            };
+
+            //Conexao com o servidor
+            var conexao = servidor.CreateConnection();
+            {
+                using(var canal = conexao.CreateModel())
+                {
+                    //Criar Fila ou escutar fila
+                    canal.QueueDeclare(queue: "fila_de_tarefas",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    Console.WriteLine("Digite uma mensagem: ");
+
+                    while (true)
+                    {
+                        string mensagem = Console.ReadLine();
+
+                        if(mensagem is "1") break;
+
+                        var corpoMensagem = Encoding.UTF8.GetBytes(mensagem);
+                        var propriedades = canal.CreateBasicProperties();
+                        propriedades.Persistent = true;
+
+                        canal.BasicPublish(exchange: "",
+                            routingKey: "fila_de_tarefas",
+                            basicProperties: propriedades,
+                            body: corpoMensagem);
+
+                        Console.WriteLine(" [x] Enviou {0}", mensagem);
+
+
+                    }
+
+                  
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var servidor = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 562,
+                UserName = "usuario",
+                Password = "Senha@123",
+            };
+
+            var con = servidor.CreateConnection();
+            {
+                using (var canal = con.CreateModel())
+                {
+                    canal.QueueDeclare(queue: "fila_de_tarefas",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    canal.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                    Console.WriteLine("[*] Aguardando mensagens.");
+
+                    var consumidor = new EventingBasicConsumer(canal);
+
+                    consumidor.Received += (model, ea) =>
+                    { 
+                        var corpo = ea.Body.ToArray();
+
+                        var mensagem = Encoding.UTF8.GetString(corpo);
+
+                        Console.WriteLine(" [x] Recebido {0}", mensagem);
+
+                        canal.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+
+                    canal.BasicConsume(queue: "fila_de_tarefas",
+                        autoAck: false,
+                        consumer: consumidor);
+
+                    Console.WriteLine("Pressione [Enter] para finalizar");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
